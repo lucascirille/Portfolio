97973644ac881151b7e519d1987844772f324f87
lucas <lucas.cirille@gmail.com>
2023-06-04 16:38:56 -0300

    better upgrades

---
+++ src/SocialNetworks.tsx
@@ -0,0 +1,36 @@
+import Github from "./components/icons/Github"
+import Instagram from "./components/icons/Instagram"
+import LinkedIn from "./components/icons/Linkedin"
+import Tweeter from "./components/icons/Tweeter"
+
+const SocialNetworks = ()=>{
+    return(
+        <>
+        <ul className="SN flex flex-col fixed align-middle left-14 bottom-0">
+        <li className="pb-5"><a href="https://github.com/lucascirille" aria-label="GitHub" target="_blank" rel="noreferrer">
+        
+        <Github/>
+
+        </a></li>
+        <li className="pb-5"><a href="https://www.instagram.com/lucas.cirille" aria-label="Instagram" target="_blank" rel="noreferrer">
+
+        <Instagram/>
+
+        </a></li>
+        <li className="pb-5"><a href="https://twitter.com/lucas_cirille" aria-label="Twitter" target="_blank" rel="noreferrer">
+
+        <Tweeter/>
+
+        </a></li>
+        <li className="pb-9"><a href="https://www.linkedin.com/in/lucas-cirille-5ba487240/" aria-label="Linkedin" target="_blank" rel="noreferrer">
+
+        <LinkedIn/>
+
+        </a></li>
+    </ul>
+
+    <div  className="fixed w-10 bottom-0 left-auto right-10 z-10 text-white"><div className="flex flex-col align-middle relative SN"><a className="my-5 mx-auto p-[10px] tracking-widest [writing-mode:vertical-rl]" href="mailto:lucas.cirille@gmail.com">lucas.cirille@gmai.com</a></div></div>
+    </>
+    )
+}
+export default SocialNetworks
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import SocialNetworks from './SocialNetworks.tsx'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('socialNetworks') as HTMLElement).render(
   <React.StrictMode>
-    {  }
-  </React.StrictMode>,
+    { 
+    <SocialNetworks/>
+      }
+  </React.StrictMode>
 )
